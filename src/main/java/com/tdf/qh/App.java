0d5dd4499f9e48fa11cd720a6277f1d90220f0dc
Asakura <chenxian_0@163.com>
2016-07-18 20:53:43 +0800

    Bean作用域、生命周期和定义继承

--- src/main/java/com/tdf/qh/App.java
+++ src/main/java/com/tdf/qh/App.java
@@ -1,5 +1,6 @@
 package com.tdf.qh;
 
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -19,28 +20,35 @@ public class App
     public static void main( String[] args )
     {
     	// 创建应用程序的上下文
-        //ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-    	// 使用XmlBeanFactory去生成bean工厂
+        //AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+    	
+        // 使用XmlBeanFactory去生成bean工厂
     	//XmlBeanFactory factory = new XmlBeanFactory((new ClassPathResource("Beans.xml")));
-    	// 使用FileSystemXmlApplicationFactory去生成bean工厂
-    	ApplicationContext context = new FileSystemXmlApplicationContext("F:/MyEclipse9/HelloSpring/src/main/java/Beans.xml");
+    	
+        // 使用FileSystemXmlApplicationFactory去生成bean工厂
+    	//ApplicationContext context = new FileSystemXmlApplicationContext("F:/MyEclipse9/HelloSpring/src/main/java/Beans.xml");
         
     	// 通过上下文获取所需的bean
-    	HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+    	//HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+    	
     	// 通过bean工厂的getBean方法获取指定bean对象
     	//HelloWorld obj = (HelloWorld) factory.getBean("helloWorld");
-        // 通过上下文获取所需的bean
-        //HelloWorld obj = (HelloWorld)context.getBean("helloWorld");
-        obj.getMessage();
-
         
+    	// 通过上下文获取所需的bean
+        //HelloWorld obj = (HelloWorld)context.getBean("helloWorld");
+        //obj.getMessage();
+
+        // Test destroy-methos
+        //context.registerShutdownHook();
         
         // Test Singleton Scope
         //testSingleton();
         
         // Test Prototype
-        testPrototype();
+        //testPrototype();
         
+        // Test Bean Parent
+        testBeanParent();
     }
     
     public static void testSingleton() {
@@ -59,5 +67,19 @@ public class App
         objA.getMessage();
         HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
         objB.getMessage();
+    }
+    
+    public static void testBeanParent() {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+    	
+    	HelloWorld objA = (HelloWorld)context.getBean("helloWorld");
+    	
+    	objA.getMessage1();
+    	objA.getMessage2();
+    	
+    	HelloIndia objB = (HelloIndia) context.getBean("helloIndia");
+    	objB.getMessage1();
+    	objB.getMessage2();
+    	objB.getMessage3();
     }
 }
--- src/main/java/com/tdf/qh/HelloWorld.java
+++ src/main/java/com/tdf/qh/HelloWorld.java
@@ -7,11 +7,37 @@ package com.tdf.qh;
  */
 public class HelloWorld {
 	private String message;
+	private String message1;
+	private String message2;
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	
 	public void getMessage() {
-		System.out.println("Your Message: " + message);
+		System.out.println("This Message: " + message);
+	}
+	public void setMessage1(String message) {
+		this.message1 = message;
+	}
+	
+	public void getMessage1() {
+		System.out.println("World Message: " + message1);
+	}
+	
+	public void setMessage2(String message) {
+		this.message2 = message;
+	}
+	
+	public void getMessage2() {
+		System.out.println("World Message: " + message2);
+	}
+	
+	// Init-method
+	public void init() {
+		System.out.println("Init before construction");
+	}
+	
+	// Destroy-method
+	public void destroy() {
+		System.out.println("Destroy after desconstruction");
 	}
 }
